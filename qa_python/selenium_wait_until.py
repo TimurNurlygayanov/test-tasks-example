2847fcd2b749f74e3dd4f02e25d31af4e23d6f09
Timur Nurlygaianov <t.nurlygaianov@semrush.com>
2018-09-19 15:23:35 +0300

    Added example of wait until clickable

---
+++ qa_python/selenium_wait_until.py
@@ -0,0 +1,14 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def test_wait_until(web_browser):
+
+    web_browser.get('https://ya.ru')
+
+    button_xpath = '//button[contains(@class, "button_theme_websearch")]'
+    button = WebDriverWait(web_browser, 10).until(
+        EC.element_to_be_clickable((By.XPATH, button_xpath)))
+
+    button.click()
