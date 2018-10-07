dcf05485a1d37da62cbf569da31e15c7bdceb3e1
Timur Nurlygaianov <t.nurlygaianov@semrush.com>
2018-10-07 16:21:02 +0300

    Finished REST API service example

---
+++ qa_python/lesson002/rest_api_service_example.py
@@ -0,0 +1,101 @@
+from uuid import uuid4
+import flask
+from flask import Flask
+from flask import request
+from flask_basicauth import BasicAuth
+
+
+app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = 'test_user'
+app.config['BASIC_AUTH_PASSWORD'] = 'test_password'
+basic_auth = BasicAuth(app)
+
+BOOKS = []
+SESSIONS = []
+
+
+def verify_cookie(req):
+    """ This function verifies cookie. """
+
+    cookie = req.cookies.get('my_cookie', '')
+
+    return bool(cookie in SESSIONS)
+
+
+@app.route('/login', methods=['GET'])
+@basic_auth.required
+def get_auth():
+    """ This function verifies user and password and creates
+        new cookie if user and password are correct.
+    """
+
+    cookie = str(uuid4())
+    SESSIONS.append(cookie)
+
+    return flask.jsonify({'auth_cookie': cookie})
+
+
+@app.route('/books', methods=['GET'])
+def get_list_of_books():
+    """ This function returns the list of books. """
+
+    if verify_cookie(request):
+        sort_filter = request.args.get('sort', '')
+        list_limit = int(request.args.get('limit', -1))
+
+        result = BOOKS
+
+        if sort_filter == 'by_title':
+            result = sorted(result, key=lambda x: x['title'])
+        if list_limit > 0:
+            result = result[:list_limit]
+
+        return flask.jsonify(BOOKS)
+
+
+@app.route('/books/<book_id>', methods=['GET'])
+def get_book(book_id):
+    """ This function returns one book from the list. """
+
+    if verify_cookie(request):
+        result = {}
+
+        for book in BOOKS:
+            if book['id'] == book_id:
+                result = book
+
+        return flask.jsonify(result)
+
+
+@app.route('/books/<book_id>', methods=['DELETE'])
+def delete_book(book_id):
+    """ This function deletes book from the list. """
+
+    global BOOKS
+
+    if verify_cookie(request):
+        result = {}
+
+        # Create new list of book and skip one book
+        # with specified id:
+        new_books = [b for b in BOOKS if b['id'] != book_id]
+
+        BOOKS = new_books
+
+        return flask.jsonify(result)
+
+
+@app.route('/add_book', methods=['POST'])
+def add_book():
+    """ This function adds new book to the list. """
+
+    if verify_cookie(request):
+        book_id = str(uuid4())
+        title = request.values.get('title', '')
+        author = request.values.get('author', 'No Name')
+
+        BOOKS.append({'id': book_id, 'title': title, 'author': author})
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', port=7000)
