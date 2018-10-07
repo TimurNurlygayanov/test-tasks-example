19ace0958885869d1b527746a3d9e8bea4c67b1e
Timur Nurlygaianov <t.nurlygaianov@semrush.com>
2018-10-07 17:17:40 +0300

    Added example of program for mock expriments

---
+++ qa_python/lesson003/product_example.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# -*- encoding=utf8 -*-
+
+# This is very simple example of some code
+# which will use files and will try call external REST API
+# so - we will try to create mock for these methods
+
+import requests
+
+
+class MySuperProgram():
+
+    def read_string_from_file(self):
+        """ This function reads first string in file and returns it. """
+
+        with open('my_file.txt', 'r') as f:
+            result = f.readline()
+
+        return result
+
+    def read_and_sort_all_strings(self):
+        """ This function reads all strings from file and returns
+            sorted list of strings.
+        """
+
+        with open('my_file.txt', 'r') as f:
+            result = f.readlines()
+
+        return result.sort()
+
+    def get_current_ip(self):
+        """ This function returns the current external ip of the host. """
+
+        res = requests.get('https://api.ipify.org/?format=json')
+        data = res.json()
+
+        return data['ip']
