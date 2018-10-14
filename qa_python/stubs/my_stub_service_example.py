376162eb9f0bee7ea83e409afa5a520ea64c70ae
Timur Nurlygaianov <t.nurlygaianov@semrush.com>
2018-10-14 12:10:19 +0300

    Added example of REST API stub

---
+++ qa_python/stubs/my_stub_service_example.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# -*- encoding=utf8 -*-
+
+# How to run:
+#  pip install -U flask
+#  python my_stub_service_example.py
+#
+
+import flask
+from flask import Flask
+from flask import request
+
+
+app = Flask(__name__)
+
+PRODUCTS = [{"id": 76699046,"name": "apple","price": 12.10}]
+
+
+def get_product():
+    """ This is very simple stub for 'get product' REST API function. """
+
+    # Default response in case
+    product = {"code": 404, "description": "Not Found"}
+
+    # Get product ID from request:
+    product_id = request.args.get('id', 0)
+
+    # Check parameters:
+    if product_id == 0:
+        product = {"code": 400,"description": "Bad Request"}
+
+    # Find the product:
+    for p in PRODUCTS:
+        if p["id"] == product_id:
+            product = p
+
+    # Return result:
+    return flask.jsonify(product)
