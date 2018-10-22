8fdac8ef1334251d8bae3478e5d2f4231d7dad5e
Timur Nurlygaianov <t.nurlygaianov@semrush.com>
2018-10-22 21:13:18 +0300

    Fixed typo

--- qa_python/lesson002/very_simple_example.py
+++ qa_python/lesson002/very_simple_example.py
@@ -6,7 +6,7 @@ user = 'test_user'
 password = 'test_password'
 
 
-def test_check_login()
+def test_check_login():
     url = 'http://localhost:7000/login'
     result = requests.get(url, auth=HTTPBasicAuth(user, password))
     assert result.status_code == 200
