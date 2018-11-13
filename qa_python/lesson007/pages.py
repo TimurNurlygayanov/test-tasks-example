914543a9cd43116f500d28c20cd9c847301440af
Timur Nurlygaianov <t.nurlygaianov@semrush.com>
2018-11-13 13:07:27 +0300

    Added magic

--- qa_python/lesson007/pages.py
+++ qa_python/lesson007/pages.py
@@ -3,23 +3,37 @@
 
 import time
 
+from selenium.webdriver import ActionChains
 from page_objects import PageObject
 from page_objects import PageElement
 from page_objects import MultiPageElement
 
 
+def wait_page_loaded(driver):
+    page_loaded = False
+
+    while not page_loaded:
+        page_loaded = driver.execute_script("return document.readyState == 'complete';")
+        time.sleep(0.1)
+
+
 class OzonMainPage(PageObject):
 
-    search_text = PageElement(class_name='search-input')
-    search_btn = PageElement(xpath='//button[@data-test-id="header-search-go"]')
+    search_text_field = PageElement(xpath='(//input)[1]')
+    search_btn = PageElement(xpath='//div[@class="bFlatButton mSearchButton"]')
 
     def __init__(self, web_driver, uri=''):
         super().__init__(web_driver, uri)
         self.get('https://www.ozon.ru/')
 
     def search(self, text):
-        self.search_text = text
-        self.search_btn.click()
+        self.search_text_field = text
+
+        action = ActionChains(self.w)
+        action.click(self.search_btn).click_and_hold(self.search_btn).\
+            pause(0.5).release(self.search_btn).perform()
+        wait_page_loaded(self.w)
+
         return OzonSearchPage(self.w)
 
 
@@ -27,14 +41,18 @@ class OzonSearchPage(PageObject):
 
     min_price = PageElement(xpath='//input[@data-test-id="range-filter-from-input"]')
     max_price = PageElement(xpath='//input[@data-test-id="range-filter-to-input"]')
-    apply_filter = PageElement(xpath='//input[@data-test-id="range-filter-show"]')
+    apply_filter = PageElement(xpath='//button[contains(text(), "Показать")]')
     products_prices = MultiPageElement(xpath='//span[@class="price-number"]/span')
-    results_links = MultiPageElement(class_name='text-link name-link')
+    results_links = MultiPageElement(xpath='//a[@class="text-link name-link"]')
     categories = MultiPageElement(xpath='//input[@data-test-id="results-navi-level2"]')
     filter_new_products = PageElement(xpath='//span[contains(text(), "Новинки")]')
     filter_popular_products = PageElement(xpath='//span[contains(text(), "Бестселлеры")]')
     filter_low_price_products = PageElement(xpath='//span[contains(text(), "Уцененный товар")]')
     filter_high_rate_products = PageElement(xpath='//span[contains(text(), "Высокий рейтинг")]')
+
+    def __init__(self, web_driver, uri=''):
+        super().__init__(web_driver, uri)
+        web_driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
 
     def has_results(self):
         return len(self.results_links) > 0
--- qa_python/lesson007/test_ozon.py
+++ qa_python/lesson007/test_ozon.py
@@ -3,11 +3,13 @@
 
 import pytest
 
+from selenium.webdriver import ActionChains
+
 from pages import OzonMainPage
 
 
 @pytest.mark.parametrize('text', ['печеньки', 'велосипед', 'ipad'])
-def test_check_search(text):
+def test_check_search(web_browser, text):
     """ Check that search page shows some products for any popular request.
 
         Steps:
@@ -16,13 +18,14 @@ def test_check_search(text):
         3) Verify that we have some results on search page
     """
 
-    main_page = OzonMainPage()
+    main_page = OzonMainPage(web_browser)
     search_page = main_page.search(text)
 
     assert search_page.has_results()
 
 
-def test_check_price_filter():
+@pytest.mark.run
+def test_check_price_filter(web_browser):
     """ Check that filters by price works fine.
 
         Steps:
@@ -32,22 +35,36 @@ def test_check_price_filter():
            and that all products have correct price
     """
 
-    main_page = OzonMainPage()
+    main_page = OzonMainPage(web_browser)
     search_page = main_page.search('книги по тестированию')
 
-    search_page.min_price = 500
+    import time
+
+    search_page.max_price.clear()
     search_page.max_price = 1000
-    search_page.apply_filter.click()
+
+    time.sleep(3)
+    search_page.min_price.clear()
+    search_page.min_price = 500
+
+
+    time.sleep(3)
+
+    action = ActionChains(search_page.w)
+    action.move_to_element(search_page.apply_filter).click_and_hold().pause(0.5).release().perform()
+    # search_page.apply_filter.click()
 
     assert search_page.has_results()
 
+    time.sleep(30)
+
     # Verify all prices on search page:
     for price in search_page.products_prices:
-        price_int = int(price.replace('&nbsp;', '').replace(' ', ''))
-        assert 500 <= price_int <= 1000, 'Wrong price: {0}'.format(price_int)
+        price_number = float(price.text.replace('&nbsp;', '').replace(' ', ''))
+        assert 500 <= price_number <= 1000, 'Wrong price: {0}'.format(price_number)
 
 
-def test_check_categories():
+def test_check_categories(web_browser):
     """ Check that all categories have some products.
 
         Steps:
@@ -56,14 +73,14 @@ def test_check_categories():
            category has some products
     """
 
-    main_page = OzonMainPage()
+    main_page = OzonMainPage(web_browser)
     search_page = main_page.search('велосипед')
 
     for category in search_page.categories:
         # Select category:
         category.click()
 
-        # Verify we have some products:
+        # Verify that we have some products:
         assert search_page.has_results(), 'Category {0} has no products!'.format(category.text)
 
         # Return to initial search results page:
