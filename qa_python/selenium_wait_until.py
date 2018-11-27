4f6fde98e496c415c0217b663ad74dfb17f460b1
Timur Nurlygaianov <t.nurlygaianov@semrush.com>
2018-11-27 14:11:49 +0300

    Added example of wait until

--- qa_python/selenium_wait_until.py
+++ qa_python/selenium_wait_until.py
@@ -1,17 +1,19 @@
-import time
+#!/usr/bin/python3
+# -*- encoding=utf8 -*-
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-def test_wait_until(web_browser):
+def test_wait_until(selenium):
     """ This is simple test case shows how to wait until
         some element will be ready for click.
     """
 
-    web_browser.get('https://ya.ru')
+    selenium.get('https://ya.ru')
 
     button_xpath = '//button[contains(@class, "button_theme_websearch")]'
-    button = WebDriverWait(web_browser, 10).until(
-        EC.element_to_be_clickable((By.XPATH, button_xpath)))
+    button = WebDriverWait(selenium, 10).until(EC.element_to_be_clickable((By.XPATH, button_xpath)))
 
     button.click()
