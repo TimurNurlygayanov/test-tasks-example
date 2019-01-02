eedf6d15f657185ea377d05d3d9b2246cffe87f6
Timur Nurlygayanov <t.nurlygayaianov@semrush.com>
2019-01-02 16:19:21 +0200

    Added script to change email

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 find . -type d -name __pycache__ -delete
 find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
 git add -A
--- inline-timer.sh
+++ inline-timer.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # This is task for yac-2018:
 while True; do clear && date | awk -F ' |:' '{print 28-$3 ":" 24+10-$4 ":" 60-$5 ":" 60-$6}' && sleep 0.5; done
 
