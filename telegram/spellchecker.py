8b4a49b7184e912ca0ee9e751e49238018caf6f5
Timur Nurlygayanov <timurnurlygayanov@mail.ru>
2019-01-25 17:55:28 +0200

    Fixed spellchecker

--- telegram/spellchecker.py
+++ telegram/spellchecker.py
@@ -8,73 +8,95 @@
 #   https://gist.github.com/Kwentar/a957d29f7370f896b691c82ff9ebe7d2
 #
 # Many thanks to the author of this code!
-# I've modified it a little bit ofr my case.
+# I've modified it a little bit for my case:
+#   1) Education now based on Russian words dictionary instead of some
+#      Russian text.
+#   2) Increased the size of the model
+#   3) Removed unnecessary Keras dependency.
 #
 
 import os
+import requests
+from termcolor import colored
+
 import gensim
-from keras.preprocessing.text import text_to_word_sequence
 import pymorphy2
 from tqdm import tqdm
 
 
-morph = pymorphy2.MorphAnalyzer()
 spell_check_model_name = 'ru_spellcheck.model'
-necessary_part = {"NOUN", "ADJF", "ADJS", "VERB", "INFN", "PRTF", "PRTS", "GRND"}
+necessary_part = {'NOUN', 'ADJF', 'ADJS', 'VERB', 'INFN',
+                  'PRTF', 'PRTS', 'GRND'}
+
+# Wrong Russian words to check the model:
+wrong_words = ['челавек',  'стулент', 'студечнеский', 'чиловенчость',
+               'учавствовать', 'тактка', 'вообщем', 'симпотичный', 'зделать',
+               'сматреть', 'алгаритм', 'ложить', 'проверка']
+# Correct words:
+words_correct = ['человек', 'студент', 'студенческий', 'человечность',
+                 'участвовать', 'тактика', 'вообще', 'симпатичный', 'сделать',
+                 'смотреть', 'алгоритм', 'положить', 'проверка']
+
+# Download Russian dictionary:
+russian_words_link = ('https://raw.githubusercontent.com/danakt/'
+                      'russian-words/master/russian.txt')
+res = requests.get(russian_words_link)
+res.encoding = 'windows-1251'
+words_for_eduction = res.text
+
+# Prepare dictionary words for eduction:
+# Note: here we use only first 100000 words, but
+# for real cases we need to use the whole list of words!
+sentences = words_for_eduction.split('\n')[:10000] + words_correct
+all_dict = sentences
 
 if not os.path.isfile(spell_check_model_name):
-    all_sentences = []
-    for filename in os.listdir('./education_texts'):
-        if filename.endswith('.txt'):
-            with open('./education_texts/{0}'.format(filename), 'r',
-                      errors='ignore') as f:
-                print('Parsing {0} ...'.format(filename))
 
-                text = f.read().split('\n')
-                sentences = []
+    morph = pymorphy2.MorphAnalyzer()
 
-                # Normalization
-                for line in text:
-                    sentences.append(text_to_word_sequence(line))
+    for i in tqdm(range(len(sentences))):
 
-                for i in tqdm(range(len(sentences))):
-                    sentence = []
-                    for el in sentences[i]:
-                        p = morph.parse(el)[0]
-                        if p.tag.POS in necessary_part:
-                            sentence.append(p.normal_form)
-                    sentences[i] = sentence
-                sentences = [x for x in sentences if x]
+        word = sentences[i]
+        sentence = []
 
-                all_sentences += sentences
+        p = morph.parse(word)[0]
+        if p.tag.POS in necessary_part:
+            sentence.append(p.normal_form)
 
-    print(len(all_sentences))
+        sentences[i] = sentence
+    sentences = [x for x in sentences if x]
 
-    # --------------------------
-    # Training
-    model = gensim.models.FastText(all_sentences, size=300, window=3, min_count=2, sg=1, iter=35)
+    # Training the model (it can take 2+ hours!):
+    model = gensim.models.FastText(sentences, size=300,
+                                   window=1, min_count=1, sg=1, iter=100,
+                                   min_n=1, max_n=6)
     model.init_sims(replace=True)
 
+    # Save model to the disk (it will take 200+ Mb on disk!)
+    print('Saving model to the disk...')
     model.save(spell_check_model_name)
 
-else:
+    print('Neuron network model is ready!')
 
-    # Just load model from file:
+else:
+    # Load model from file:
     model = gensim.models.FastText.load(spell_check_model_name)
     model.init_sims(replace=True)
 
-    words = ['челавек',  'стулент', 'студечнеский', 'чиловенчость',
-             'учавствовать', 'тактка', 'вообщем', 'симпотичный', 'зделать',  'сматреть', 'алгаритм', 'ложить']
-    words_correct = ['человек', 'студент', 'студенческий', 'человечность',
-                     'участвовать', 'тактика', 'вообще', 'симпатичный', 'сделать',
-                     'смотреть', 'алгоритм', 'положить']
 
-    for index, word in enumerate(words):
-        if word in model:
-            print(word)
+spell_checker = model.wv
 
-            for i in model.most_similar(positive=[word], topn=20):
-                print(i[0], i[1])
-            print('---------------')
-            print(model.similarity(word, words_correct[index]))
-            print('______________\n')
+# Check what options model will suggest to correct each wrong word:
+for index, word in enumerate(wrong_words):
+
+    if word in all_dict:
+        print('\n ---- \n\n ', colored(word, 'green'))
+        print('The word is correct!')
+    else:
+        print('\n ---- \n\n ', colored(word, 'red'))
+
+        print('Suggested options:')
+        for i in spell_checker.most_similar(positive=[word])[:5]:
+            print(colored(i[0], 'yellow'), i[1])
+
+print('\n ---- \n\n ')
