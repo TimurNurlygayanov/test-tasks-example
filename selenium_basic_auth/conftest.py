a2f65df6ac46b38b8bf7f64d1ae0ee1b57689e7b
Timur Nurlygaianov <t.nurlygaianov@semrush.com>
2021-01-11 16:04:26 +0200

    Added nosandbox

--- selenium_basic_auth/conftest.py
+++ selenium_basic_auth/conftest.py
@@ -4,5 +4,6 @@ import pytest
 @pytest.fixture
 def chrome_options(chrome_options):
     # chrome_options.add_extension('basic_auth_plugin')
+    chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--load-extension=./basic_auth_plugin')
     return chrome_options
