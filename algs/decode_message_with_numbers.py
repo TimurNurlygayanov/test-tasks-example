37bc7a0c29b2a302f3783e93a78d1bc2f45ba5ba
Timur Nurlygaianov <t.nurlygaianov@semrush.com>
2021-02-28 19:18:11 +0200

    Added more optimal solution for the task to decode message

--- algs/decode_message_with_numbers.py
+++ algs/decode_message_with_numbers.py
@@ -1,7 +1,8 @@
 # The task was taken from
 # https://www.youtube.com/watch?v=qli-JCrSwuk
 # I've spent several hours to solve it..
-#
+# and then just 10 minutes for the new solution
+# after I watched the full video..
 
 
 DECODE_TABLE = {str(i): chr(k) for i, k
@@ -21,9 +22,9 @@ def generate_options(msg: str) -> list:
     if len(msg) == 2:
         return [msg, msg[0] + '+' + msg[1]]
 
-    options = generate_options(msg[1:])
     result = []
 
+    options = generate_options(msg[1:])
     for o in options:
         num = o.split('+')
         if len(num[0]) == 1:
@@ -33,7 +34,6 @@ def generate_options(msg: str) -> list:
         result.append(num)
 
     options = generate_options(msg[2:])
-
     for o in options:
         num = msg[:2] + '+' + o
         result.append(num)
@@ -47,7 +47,26 @@ def count_all_options(msg: str) -> int:
     return len(result)
 
 
+def helper(msg: str, k: int) -> int:
+    if k == 0 or k == 1:
+        return 1
+    if msg[0] == '0':
+        return 0
+
+    s = len(msg) - k
+    result = helper(msg, k-1)
+
+    if int(msg[s:s+2]) < 27:
+        result += helper(msg, k-2)
+
+    return result
+
+
+def count_number_of_options(msg: str) -> int:
+    return helper(msg, len(msg))
+
+
 examples = ['12', '123', '01', '123231', '99', '911']
 for sample in examples:
-    result = count_all_options(sample)
+    result = count_number_of_options(sample)
     print('{0} -> {1}'.format(sample, result))
